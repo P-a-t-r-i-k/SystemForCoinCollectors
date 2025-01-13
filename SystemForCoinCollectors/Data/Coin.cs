8f750a8e7957d07426dca64ef67590dea3b1a15a
P-a-t-r-i-k <patrik.lasso.ml@gmail.com>
2025-01-13 19:48:11 +0100

    I changed Coin table and page for creating and editing coins.

--- SystemForCoinCollectors/Data/Coin.cs
+++ SystemForCoinCollectors/Data/Coin.cs
@@ -6,7 +6,9 @@
         public required string Feature { get; set; }
         public string Description { get; set; }
         public string IssuingVolume { get; set; }
-        public string IssuingDate { get; set; }
+        public string IssuingYear { get; set; }
+        public string Image { get; set; }
+        public string Country { get; set; }
 
     }
 }
--- SystemForCoinCollectors/Migrations/20250113182048_ChangeCoinTable.Designer.cs
+++ SystemForCoinCollectors/Migrations/20250113182048_ChangeCoinTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SystemForCoinCollectors.Data;
 
@@ -11,9 +12,11 @@ using SystemForCoinCollectors.Data;
 namespace SystemForCoinCollectors.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250113182048_ChangeCoinTable")]
+    partial class ChangeCoinTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
